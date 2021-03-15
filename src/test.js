93a093d33e0d276272fe323d8d44c739a83af7fc
MiaMou <19845509@qq.com>
2021-03-15 17:09:22 +0800

    init

--- src/test.js
+++ src/test.js
@@ -1,4 +1,6 @@
-const string = `
+const string = `/* 画一个海绵宝宝
+* 首先画一个背景
+*/
 .skin * {
     box-sizing: border-box;
     margin: 0;
@@ -10,185 +12,282 @@ const string = `
   }
   .skin {
     background: #ffe600;
-    min-height: 50vh;
+    min-height: 60vh;
     position: relative;
   }
+  /* 接下来画鼻子
+ */
   .nose {
-    border: 10px solid black;
-    border-color: #000 transparent transparent;
-    border-bottom: none;
-    width: 0px;
-    height: 0px;
-    position: relative;
+    width: 70px; 
+    height: 100px;
+    position: absolute;
+    border-radius: 80%;
     left: 50%;
-    top: 145px;
-    margin-left: -10px;
-    z-index: 10;
+    top: 200px;
+    background-color:#000;
+    margin-left: -35px;
+    z-index: 2;
   }
-  @keyframes wave {
-    0% {
-      transform: rotate(0deg);
-    }
-    33% {
-      transform: rotate(5deg);
-    }
-    66% {
-      transform: rotate(-5deg);
-    }
-    100% {
-      transform: rotate(0deg);
-    }
+  .nose::after {
+    position: absolute;
+    content: " ";
+    width:60px; 
+    height:100px;
+    background:#ffe600;
+    border-radius: 80%;
+    left: 5px;
+    top: 5px;
+    z-index:5;
   }
-  .nose:hover {
-    transform-origin: center bottom;
-    animation: wave 230ms infinite linear;
-  }
-  .arc {
-    position: relative;
-    width: 20px;
-    height: 6px;
-    /* border: 1px solid green; */
-    top: -16px;
-    left: -10px;
-    border-radius: 10px 10px 0 0;
-    background: #000;
-  }
+  /* 接下来画眼睛
+  */
   .eye {
-    border: 3px solid #000;
-    width: 64px;
-    height: 64px;
+    width: 140px;
+    height: 140px;
     position: absolute;
     left: 50%;
-    top: 100px;
-    margin-left: -32px;
-    background: #2e2e2e;
-    border-radius: 50%;
-  }
-  .eye::before {
-    content: "";
-    display: block;
-    border: 3px solid #000;
-    width: 31px;
-    height: 31px;
+    top: 110px;
+    margin-left: -70px;
     background: #fff;
     border-radius: 50%;
-    position: relative;
-    left: 7px;
-    top: -1px;
+    border: 6px solid #000;
   }
-  .eye.left {
+  .eye.left{
+    transform: translateX(-70px);
+  }
+  .eye.right{
+    transform: translateX(70px);
+  }
+  .eye.left::before { 
+    content: '';
+    border: 5px solid #000; 
+    width: 60px;
+    height: 60px;
+    display: block;
+    border-radius: 50%; 
+    background: #64c5f2;
+    position: relative;
+    top: 30px;
+    left: 50px; 
+  }
+  .eye.left::after {
+    content: " ";
+    width: 30px;
+    height: 30px;
+    display: block;
+    background-color: #000;
+    border-radius:50%;
+    position: absolute;
+    top: 46px;
+    left: 66px;  
+  }
+  .eye.right::before { 
+    content: '';
+    border: 5px solid #000; 
+    width: 60px;
+    height: 60px;
+    display: block;
+    border-radius: 50%; 
+    background: #64c5f2;
+    position: relative;
+    top: 30px;
+    left: 20px; 
+  }
+  .eye.right::after {
+    content: " ";
+    width: 30px;
+    height: 30px;
+    display: block;
+    background-color: #000;
+    border-radius:50%;
+    position: absolute;
+    top: 46px;
+    left: 34px;  
+  }
+  /* 接下来画眼睫毛
+  */
+  .eyelashes.left {
+    position: absolute;
+    top: -30px;
+      left: 60px;
+    width: 8px;
+    height: 25px;
+    background: #000;
+  }
+  .eyelashes.left::before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 5px;
+      left: 30px;
+      width: 8px;
+    height: 25px;
+      background: #000;
+    transform: rotate(30deg);
+  }
+  .eyelashes.left::after {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 5px;
+      left: -30px;
+      width: 8px;
+    height: 25px;
+      background: #000;
+      transform: rotate(-30deg);
+  }
+  .eyelashes.right {
+    position: absolute;
+    top: -30px;
+      left: 60px;
+    width: 8px;
+    height: 25px;
+    background: #000;
+  }
+  .eyelashes.right::before {
+    content: '';
+      display: block;
+      position: absolute;
+      top: 5px;
+      left: -30px;
+      width: 8px;
+    height: 25px;
+      background: #000;
+      transform: rotate(-30deg);
+  }
+  .eyelashes.right::after {
+    content: '';
+      display: block;
+      position: absolute;
+      top: 5px;
+      left: 30px;
+      width: 8px;
+    height: 25px;
+      background: #000;
+      transform: rotate(30deg);
+  }
+  /* 接下来画脸颊和小雀斑
+  */
+  .face {
+    width: 60px;
+      height: 60px;
+      position: absolute;
+    left: 50%;
+    top: 110px;
+    margin-left: -30px;
+    margin-top: 100px;
+      border: 5px solid #fa3c14;
+      border-radius: 50px;
+      background: #ffe600;
+  }
+  .face.left{
     transform: translateX(-120px);
   }
-  .eye.right {
+  .face.right{
     transform: translateX(120px);
   }
-  .mouth {
-    /* border: 1px solid red; */
-    width: 200px;
-    height: 200px;
+  .face::before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 10%;
+      width: 40px;
+      height: 40px;
+      background: #ffe600;
+      z-index: 2;
+  }
+  .spot {
+      display: block;
+      position: absolute;
+      left: 15%;
+      top: 30%;
+      width: 10px;
+      height: 10px;
+      background-color: #fa3c14;
+      border-radius: 50%;
+      z-index: 5;
+  }
+  .spot::before {
+      content: '';
+      display: block;
+      position: absolute;
+      left: 15px;
+    top: -10px;
+      width: 10px;
+      height: 10px;
+      background-color: #fa3c14;
+      border-radius: 50%;
+      z-index: 5;
+  }
+  .spot::after {
+      content: '';
+      display: block;
+      position: absolute;
+      left: 30px;
+      width: 10px;
+      height: 10px;
+      background-color: #fa3c14;
+      border-radius: 50%;
+      z-index: 5;
+  }
+  /* 接下来画一个微笑的嘴巴
+  */
+  .smile {
+      width: 300px;
+      height: 120px;
     position: absolute;
     left: 50%;
-    top: 170px;
-    margin-left: -100px;
-  }
-  .mouth .up {
-    position: relative;
-    top: -20px;
-    z-index: 1;
-  }
-  .mouth .up .lip {
-    border: 3px solid black;
-    height: 30px;
-    width: 100px;
-    background: #ffe600;
-    border-top-color: transparent;
-    position: relative;
-    position: absolute;
-    left: 50%;
-    margin-left: -50px;
-  }
-  .mouth .up .lip.left {
-    border-radius: 0 0 0 50px;
-    transform: rotate(-15deg) translateX(-53px);
-  }
-  .mouth .up .lip::before {
-    content: "";
-    display: block;
-    width: 7px;
-    height: 30px;
-    /* border: 1px solid green; */
-    position: absolute;
-    bottom: 0;
-    background: #ffe600;
-  }
-  .mouth .up .lip.left::before {
-    right: -6px;
-  }
-  .mouth .up .lip.right {
-    border-radius: 0 0 50px 0;
-    transform: rotate(15deg) translateX(53px);
-  }
-  .mouth .up .lip.right::before {
-    left: -6px;
-  }
-  .mouth .down {
-    height: 180px;
-    position: absolute;
-    top: 5px;
-    width: 100%;
-    overflow: hidden;
-  }
-  .mouth .down .yuan1 {
-    border: 3px solid black;
-    width: 150px;
-    height: 1000px;
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    margin-left: -75px;
-    border-radius: 75px/300px;
-    background: #9b000a;
-    overflow: hidden;
-  }
-  .mouth .down .yuan1 .yuan2 {
-    background: #ff485f;
-    width: 200px;
-    height: 300px;
-    position: absolute;
-    bottom: -155px;
-    left: 50%;
-    margin-left: -100px;
-    border-radius: 100px;
-  }
-  .face {
-    position: absolute;
-    left: 50%;
-    border: 3px solid #000;
-    width: 88px;
-    height: 88px;
     top: 200px;
-    margin-left: -44px;
-    z-index: 3;
+    margin-left: -150px;
+      border: 5px solid #000;
+      border-color: #000 transparent transparent transparent;
+      border-radius: 50% / 100px 100px 10px 10px;
+      transform: rotate(180deg);
+      z-index: 2;
   }
-  .face > img {
+  .smile::before {
+      content: '';
+      position: absolute;
+      top: 15px;
+      left: 28px;
+      width: 20px;
+      height: 24px;
+    border: 5px solid transparent;
+    border-left: 5px solid #000;
+    transform: rotate(-30deg);
+  }
+  .smile::after {
+      content: '';
+      position: absolute;
+      top: 24px;
+      right: 18px;
+      width: 20px;
+      height: 24px;
+    border: 5px solid transparent;
+    border-left: 5px solid #000;
+    transform: rotate(30deg);
+  }
+  /* 最后画两颗大门牙
+  */
+  .teeth {
+    width: 50px;
+      height: 50px;
     position: absolute;
-    top: 50%;
     left: 50%;
+    top: 315px;
+    margin-left: -25px;
+      background: white;
+      border-radius: 5px;	
+      border: 5px solid #000;
   }
-  .face.left {
-    transform: translateX(-180px);
-    border-radius: 50%;
-    background: #ff0000;
+  .teeth.left {
+      transform: translateX(-30px) rotate(5deg);
   }
-  .face.left > img {
-    transform: rotateY(180deg);
-    transform-origin: 0 0;
-  }
-  .face.right {
-    transform: translateX(180px);
-    border-radius: 50%;
-    background: #ff0000;
-  }  
+  .teeth.right {
+      transform: translateX(30px) rotate(-5deg);
+  } 
+  /* 大功告成！
+  */ 
 `
 const player = {
     id: undefined,
